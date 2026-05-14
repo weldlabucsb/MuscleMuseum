7f04806535f4f935c13ea5a653338599ee266a77
XiaoC <xiaochai.joe@gmail.com>
2026-05-14 16:00:39 -0700

    updated computeKd. Leave the 2D average calculation in the comments

--- src/trial/becExp/func/computeKd.m
+++ src/trial/becExp/func/computeKd.m
@@ -36,16 +36,11 @@ end
 
 if ~isnan(cloudWidth)
     if ~isa(ol.Laser,"GaussianBeam")
-       error("ol's Lasre property must be GaussianBeam.") 
+       error("ol's Laser must be GaussianBeam.") 
     end
     if isnan(ol.Laser.Waist(1))
         error("The beam waist of the optical lattice has to be provided.")
     end
-    nx = 1e3;
-    x = linspace(-5 * cloudWidth,5 * cloudWidth,nx);
-    density1D = boseFunctionApprox(exp(-x.^2/(cloudWidth^2)),2.5);
-    density1D = density1D./sum(density1D);
-    gaussianBeam1D = exp(-2 * x.^2/prod(ol.Laser.Waist));
 
     % Interpolate
     KdInterp = cell(1,nDim);
@@ -54,10 +49,30 @@ if ~isnan(cloudWidth)
         KdInterp{nn} = @(q) interp1(V0List, nthOrder, q, 'pchip', 'extrap');
     end
 
+    nx = 1e3;
+    x = linspace(-5 * cloudWidth,5 * cloudWidth,nx);
+    density1D = boseFunctionApprox(exp(-x.^2/(cloudWidth^2)),2.5);
+    density1D = density1D./sum(density1D);
+    gaussianBeam1D = exp(-2 * x.^2/prod(ol.Laser.Waist));
+
     % Compute weighted average
     pop = cell2mat(arrayfun(@(jj) ...
         sum(cell2mat(arrayfun(@(ii) density1D(ii) * KdInterp{jj}(gaussianBeam1D(ii) * V0List),(1:nx),UniformOutput=false)),2),...
         (1:nDim),UniformOutput=false));
+
+    % nx = 1e2;
+    % x = linspace(-5 * cloudWidth,5 * cloudWidth,nx);
+    % [X,Y] = meshgrid(x,x);
+    % density2D = boseFunctionApprox(exp(-(X.^2+Y.^2)/(cloudWidth^2)),2);
+    % density2D = density2D./sum(density2D(:));
+    % gaussianBeam2D = exp(-2 * (X.^2+Y.^2)/prod(ol.Laser.Waist));
+    % 
+    % % Compute weighted average
+    % for ii = 1:nDim
+    %     pop(:,ii) = arrayfun(@(kk) ...
+    %         sum(density2D.* KdInterp{ii}(gaussianBeam2D * V0List(kk)),"all"),...
+    %         (1:size(V0List)).');
+    % end
 end
 
 if isPlot
